daa7e7b31164aaeaecd2ad4cda0076540afa2e4e
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-05-28 18:26:57 -0300

    add: Configurando settings e criando modelo de dados (models.py)

---
+++ register_patient/choices.py
@@ -0,0 +1,16 @@
+SEXO_CHOICES = (
+    ("F", "Feminino"),
+    ("M", "Masculino"),
+    ("N", "Nenhuma das opções")
+)
+
+CONVENIOS_CHOICES = (
+    ('SulAmérica', 'SulAmérica'),
+    ('NotreDame Intermédica', 'NotreDame Intermédica'),
+    ('Prevent Senior', 'Prevent Senior'),
+    ('Assim Saúde', 'Assim Saúde'),
+    ('Central Nacional Unimed (CNU)', 'Central Nacional Unimed (CNU)'),
+    ('GreenLine Sistema de Saúde', 'GreenLine Sistema de Saúde'),
+    ('Bradesco Seguros', 'Bradesco Seguros'),
+    ('Hapvida Saúde', 'Hapvida Saúde')
+)
--- register_patient/models.py
+++ register_patient/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+from register_patient.choices import SEXO_CHOICES, CONVENIOS_CHOICES
+
+
+class Paciente(models.Model):
+    prontuario = models.IntegerField(max_length=20, verbose_name='Prontuário')
+    nome = models.CharField(max_length=100, verbose_name='Nome')
+    sobrenome = models.CharField(max_length=100, verbose_name='Sobrenome')
+    data_nasci = models.DateField(auto_now_add=False, verbose_name='Data de Nascimento')
+    genero = models.CharField(max_length=1, choices=SEXO_CHOICES, blank=False, null=False, verbose_name='Gênero')
+
+    cpf = models.CharField(max_length=11, verbose_name='CPF', unique=True)
+    rg = models.CharField(max_length=11, verbose_name='CPF', unique=True)
+    uf_rg = models.IntegerField(verbose_name='UF do CPF')
+    email = models.EmailField(verbose_name='Email')
+    celular = models.CharField(max_length=12, unique=True, verbose_name='Nº telefone celular')
+    tel_fixo = models.CharField(max_length=12, verbose_name='Nº telefone fixo')
+
+    convenio = models.CharField(max_length=50, choices=CONVENIOS_CHOICES, blank=False, null=False, verbose_name='Convênio')
+    carteira_convenio = models.IntegerField(verbose_name='Carteira convênio')
+    val_carteirinha = models.CharField(max_length=5, verbose_name='Validade Carteira')
+
+    def __str__(self):
+        return str(self.nome) + str(self.sobrenome)
