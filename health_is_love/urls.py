ddc62b7cc39eb43b73c47f87001914656019c243
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-05-31 12:03:09 -0300

    add: criando editor de registro dos pacientes

--- health_is_love/urls.py
+++ health_is_love/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from register_patient.views import PacienteViewCBV, create_paciente
+from register_patient.views import PacienteViewCBV, create_pac, update_pac
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('listagem/', PacienteViewCBV.as_view(), name='list'),
-    path('novo/', create_paciente, name='create'),
-    # path('adicionar/', PacienteAddCBV.as_view())
+    path('novo/', create_pac, name='create'),
+    path('editar/<int:pk>', update_pac, name='update'),
 ]
--- register_patient/views.py
+++ register_patient/views.py
@@ -10,7 +10,7 @@ class PacienteViewCBV(ListView):
     template_name = 'list_pac.html'
 
 
-def create_paciente(request):
+def create_pac(request):
     try:
         if request.POST:
             Paciente.objects.create(
@@ -34,7 +34,37 @@ def create_paciente(request):
     except Exception as err:
         return render(request, 'create_pac.html', {'error': f'Não foi possivel salvar o novo paciente, Por favor, '
                                                             f'tente novamente com as informações corretas. {err}'})
-    # finally:
-    #     print(request.POST['prontuario'])
 
     return render(request, 'create_pac.html')
+
+
+def update_pac(request, pk):
+    pac = Paciente.objects.get(pk=pk)
+
+    if request.method == 'GET':
+        return render(request, 'update_pac.html', {'pac': pac})
+    else:
+        try:
+            Paciente.objects.filter(pk=pk).update(
+                prontuario=request.POST['prontuario'],
+                nome=request.POST['nome'],
+                sobrenome=request.POST['sobrenome'],
+                data_nasci=request.POST['data_nasci'],
+                genero=request.POST['genero'],
+                cpf=request.POST['cpf'],
+                rg=request.POST['rg'],
+                uf_rg=request.POST['uf_rg'],
+                email=request.POST['email'],
+                celular=request.POST['celular'],
+                tel_fixo=request.POST['tel_fixo'],
+                convenio=request.POST['convenio'],
+                carteira_convenio=request.POST['carteira_convenio'],
+                val_carteirinha=request.POST['val_carteirinha'],
+            )
+            return render(request, 'update_pac.html', {'save': 'Alteração Salva com sucesso', 'pac': pac})
+        except Exception as err:
+            return render(request, 'update_pac.html', {'error': f'Não foi possivel salvar o novo paciente, Por favor, '
+                                                                f'tente novamente com as informações corretas. {err}',
+                                                       'pac': pac})
+
+
