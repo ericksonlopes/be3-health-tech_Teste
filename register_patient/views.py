cf8c3b8ec71b49ed886822636650cfe3c072262f
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-05-31 23:57:09 -0300

    fix: bug da divisão por 0 consertado. add: card que exibe a quantidade de pacientes cadastrados

--- register_patient/views.py
+++ register_patient/views.py
@@ -4,13 +4,12 @@ from register_patient.models import Paciente
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+
 class PacienteViewCBV(LoginRequiredMixin, ListView):
     model = Paciente
     context_object_name = 'paciente'
     # Template especificado
     template_name = 'list_pac.html'
-
-
 
 
 @login_required
@@ -100,8 +99,15 @@ def homepage(request):
     list_conv = ['SulAmérica', 'NotreDame Intermédica', 'Prevent Senior', 'Assim Saúde',
                  'Central Nacional Unimed (CNU)', 'GreenLine Sistema de Saúde', 'Bradesco Seguros']
 
-    return render(request, 'home.html', {'len': len(Paciente.objects.all()), 'porc_conv':
-        [{'qtd': len(Paciente.objects.filter(convenio=conv)),
-          'conv': conv,
-          'porc': f'{(len(Paciente.objects.filter(convenio=conv)) / len(Paciente.objects.all())) * 100:.3}'
-          } for conv in list_conv]})
+    if len(Paciente.objects.all()) == 0:
+        porc_conv = [{'qtd': 0,
+                      'conv': conv,
+                      'porc': 0
+                      } for conv in list_conv]
+    else:
+        porc_conv = [{'qtd': len(Paciente.objects.filter(convenio=conv)),
+                      'conv': conv,
+                      'porc': f'{(len(Paciente.objects.filter(convenio=conv)) / len(Paciente.objects.all())) * 100:.4}'
+                      } for conv in list_conv]
+
+    return render(request, 'home.html', {'len': len(Paciente.objects.all()), 'porc_conv': porc_conv})
