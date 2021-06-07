72e9fa1be98f1c0ad4a12767bf56869708d4425c
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-06-07 16:52:32 -0300

    add: criar e atualizar consultas

--- appointment/urls.py
+++ appointment/urls.py
@@ -3,6 +3,6 @@ from .views import list_appo, create_appo, update_appo
 
 urlpatterns = [
     path('list_appo/', list_appo, name='list-appo'),
-    path('add-appo/', create_appo, name='create-appo'),
+    path('add-appo/<int:pk>', create_appo, name='create-appo'),
     path('editar-appo/<int:pk>', update_appo, name='update-appo'),
 ]
--- appointment/views.py
+++ appointment/views.py
@@ -2,18 +2,54 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from appointment.models import Appointment
 from datetime import date
+from register_patient.models import Paciente
 
 
 @login_required
-def create_appo(request):
+def create_appo(request, pk):
     if request.method == 'GET':
-        return render(request, 'appointment/create_appo.html')
+        return render(request, 'appointment/create_appo.html', {'pac': Paciente.objects.get(pk=pk)})
+    try:
+        if request.POST:
+            Appointment.objects.create(
+                pacient=Paciente.objects.get(pk=pk),
+                date=request.POST['date'],
+                time=request.POST['time'],
+                doctorName=request.POST['doctorName']
+            )
+            return render(request, 'appointment/create_appo.html', {'save': 'Salvo com sucesso',
+                                                                    'pac': Paciente.objects.get(pk=pk)})
+
+    except Exception as err:
+
+        return render(request, 'appointment/create_appo.html', {'error': err, 'pac': Paciente.objects.get(pk=pk)})
 
 
 @login_required
 def update_appo(request, pk):
     if request.method == 'GET':
-        return render(request, 'appointment/update_appo.html', {'appo': Appointment.objects.get(pk=pk)})
+        return render(request, 'appointment/update_appo.html',
+                      {'pac': Paciente.objects.get(pk=Appointment.objects.get(pk=pk).pacient.id),
+                       'appo': Appointment.objects.get(pk=pk)})
+
+    try:
+        if request.POST:
+            Appointment.objects.filter(pk=pk).update(
+                pacient=Paciente.objects.get(pk=Appointment.objects.get(pk=pk).pacient.id),
+                date=request.POST['date'],
+                time=request.POST['time'],
+                doctorName=request.POST['doctorName']
+            )
+            return render(request, 'appointment/update_appo.html', {'save': 'Atualizado com sucesso',
+                                                                    'pac': Paciente.objects.get(
+                                                                        pk=Appointment.objects.get(pk=pk).pacient.id),
+                                                                    'appo': Appointment.objects.get(pk=pk)})
+
+    except Exception as err:
+        return render(request, 'appointment/create_appo.html', {'error': err,
+                                                                'pac': Paciente.objects.get(
+                                                                    pk=Appointment.objects.get(pk=pk).pacient.id),
+                                                                'appo': Appointment.objects.get(pk=pk)})
 
 
 @login_required
