e9ace7f921a5076331d5f34a92677c60b79f129d
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-06-07 08:58:18 -0300

    ajustando linguagem e horario padrão

--- appointment/views.py
+++ appointment/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from appointment.models import Appointment
+from datetime import date
 
 
 # Create your views here.
 def list_appo(request):
-    return render(request, 'appointment/list_appo.html', {'appo': Appointment.objects.all()})
+    return render(request, 'appointment/list_appo.html', {'appo': Appointment.objects.all(), 'datenow': date.today()})
