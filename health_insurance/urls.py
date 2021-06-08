fa6b91167119a65048d195aec347048dde9d6e4a
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-06-08 09:31:24 -0300

    criar convenio

--- health_insurance/urls.py
+++ health_insurance/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import list_hi
+from .views import list_hi, create_hi
 
 urlpatterns = [
     path('list-hi/', list_hi, name='list-hi'),
+    path('create-hi/', create_hi, name='create-hi'),
 ]
 
 
--- health_insurance/views.py
+++ health_insurance/views.py
@@ -6,3 +6,18 @@ from .models import HealthInsurance
 @login_required
 def list_hi(request):
     return render(request, 'health_insurance/list_hi.html', {'hi': HealthInsurance.objects.all()})
+
+
+@login_required
+def create_hi(request):
+    if request.method == 'GET':
+        return render(request, 'health_insurance/create_hi.html')
+
+    try:
+        if request.POST:
+            HealthInsurance.objects.create(
+                name=request.POST['name']
+            )
+        return render(request, 'health_insurance/create_hi.html', {'save': 'Convênio salvo com sucesso'})
+    except Exception as err:
+        return render(request, 'health_insurance/create_hi.html', {'error': err})
