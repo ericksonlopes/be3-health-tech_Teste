0a04b759dfd4181ad4fa5aa48a3705c79f6c360a
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-06-08 09:39:40 -0300

    add: Atualizar Convênios

--- health_insurance/urls.py
+++ health_insurance/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
-from .views import list_hi, create_hi
+from .views import list_hi, create_hi, update_hi
 
 urlpatterns = [
     path('list-hi/', list_hi, name='list-hi'),
     path('create-hi/', create_hi, name='create-hi'),
+    path('update-hi/<int:pk>', update_hi, name='update-hi'),
 ]
 
 
--- health_insurance/views.py
+++ health_insurance/views.py
@@ -21,3 +21,19 @@ def create_hi(request):
         return render(request, 'health_insurance/create_hi.html', {'save': 'Convênio salvo com sucesso'})
     except Exception as err:
         return render(request, 'health_insurance/create_hi.html', {'error': err})
+
+
+def update_hi(request, pk):
+    if request.method == 'GET':
+        return render(request, 'health_insurance/update_hi.html', {'hi': HealthInsurance.objects.get(pk=pk)})
+
+    try:
+        if request.POST:
+            HealthInsurance.objects.filter(pk=pk).update(
+                name=request.POST['name']
+            )
+        return render(request, 'health_insurance/update_hi.html', {'save': 'Atualizado com sucesso',
+                                                                   'hi': HealthInsurance.objects.get(pk=pk)})
+    except Exception as err:
+        return render(request, 'health_insurance/update_hi.html', {'error': err,
+                                                                   'hi': HealthInsurance.objects.get(pk=pk)})
